0020b3cb32bb2755123c7edacd84a19115b62adc
satish rajput <satishetc4288@gmail.com>
2024-08-16 21:53:09 +0530

    added amqp plugin

---
+++ src/main/java/com/satish/exp/config/AmqpSubscriber.java
@@ -0,0 +1,14 @@
+package com.satish.exp.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AmqpSubscriber {
+
+    @RabbitListener(queues = "test-java-queue")
+    public void processMessage(String content) {
+        System.out.println("########### message received " + content);
+    }
+
+}
